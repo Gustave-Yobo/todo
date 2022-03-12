e262ef21c698ad1140186268b2440fb614f43a97
Gustave-Yobo <gustaveyoboyann@gmail.com>
2022-03-12 03:59:41 +0100

    petite application de test todolist

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('tasks/create', [TaskController::class, 'store'])->name('tasks.store');
+
+    Route::get('tasks/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('tasks/update', [TaskController::class, 'update'])->name('tasks.update');
+    Route::get('tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::delete('tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 });
